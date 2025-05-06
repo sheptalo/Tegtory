4c3a38d244748e12cd214ef846abae4235d28610
shept <github@sinortax.ru>
2025-05-06 17:09:54 +0300

    added mypy to CI

--- presenters/shared/utils/__init__.py
+++ presenters/shared/utils/__init__.py
@@ -1,10 +1,8 @@
 from .auth import get_factory, get_storage_from_factory
 from .cache import cache
-from .context import with_context
 
 __all__ = [
     "cache",
     "get_factory",
     "get_storage_from_factory",
-    "with_context",
 ]
